27e679e527c3a84517575df974a08d82d831bd0f
danigb <danigb@gmail.com>
2010-10-19 01:13:03 +0200

    Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asamblea::Application.config.secret_token = '9271d123f1fc6d8f48194fbb0826324f36d3c52c83a0493091b62e7c78c146571f3e1d2298dd51896e192546d2ea876f5adb3ded0a0ce6abf2b0b219a3ea799c'
